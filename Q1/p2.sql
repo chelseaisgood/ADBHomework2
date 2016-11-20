561e0b5ced0ad6f580ee87a2cea1333ecb8688dd
chelseaisgood <chelseaisgoodfmd@gmail.com>
2016-11-20 15:12:49 -0500

    updates by 2016-11-20

--- Q1/p2.sql
+++ Q1/p2.sql
@@ -1,6 +1,39 @@
-select stocksymbol, avg(price)
-from stock join
-	(select stocksymbol, price
-	  from stock
-	  limit 10) as stock1
-group by stocksymbol
+select @total = 70002;
+set @i = 1;
+
+drop table if exists temp1;
+drop table if exists temp2;
+drop table if exists result;
+set @row = 0;
+
+create temporary table result (stocksymbol int, running_average double);
+#create table temp1 as (select (@row:=@row + 1) as num, stocksymbol, quantity, price from stock where stocksymbol in (select distinct stocksymbol from stock) );
+create table temp1 as (select (@row:=@row + 1) as num, stocksymbol, quantity, price from stock where stocksymbol = 1);
+create table temp2 as (select * from temp1);
+select * from temp1;
+select * from temp2;
+
+select stocksymbol, avg(sub.price) as running_average
+from
+(
+select temp1.stocksymbol, temp1.num, temp2.price from temp1, temp2 where (temp1.stocksymbol = temp2.stocksymbol) and (temp1.num - 9 = temp2.num)
+union
+select temp1.stocksymbol, temp1.num, temp2.price from temp1, temp2 where (temp1.stocksymbol = temp2.stocksymbol) and (temp1.num - 8 = temp2.num)
+union
+select temp1.stocksymbol, temp1.num, temp2.price from temp1, temp2 where (temp1.stocksymbol = temp2.stocksymbol) and (temp1.num - 7 = temp2.num)
+union
+select temp1.stocksymbol, temp1.num, temp2.price from temp1, temp2 where (temp1.stocksymbol = temp2.stocksymbol) and (temp1.num - 6 = temp2.num)
+union
+select temp1.stocksymbol, temp1.num, temp2.price from temp1, temp2 where (temp1.stocksymbol = temp2.stocksymbol) and (temp1.num - 5 = temp2.num)
+union
+select temp1.stocksymbol, temp1.num, temp2.price from temp1, temp2 where (temp1.stocksymbol = temp2.stocksymbol) and (temp1.num - 4 = temp2.num)
+union
+select temp1.stocksymbol, temp1.num, temp2.price from temp1, temp2 where (temp1.stocksymbol = temp2.stocksymbol) and (temp1.num - 3 = temp2.num)
+union
+select temp1.stocksymbol, temp1.num, temp2.price from temp1, temp2 where (temp1.stocksymbol = temp2.stocksymbol) and (temp1.num - 2 = temp2.num)
+union
+select temp1.stocksymbol, temp1.num, temp2.price from temp1, temp2 where (temp1.stocksymbol = temp2.stocksymbol) and (temp1.num - 1 = temp2.num)
+union
+select temp1.stocksymbol, temp1.num, temp2.price from temp1, temp2 where (temp1.stocksymbol = temp2.stocksymbol) and (temp1.num = temp2.num)
+) as sub
+group by stocksymbol, num;
