21723b07f7daaad6c66c57fd16164f7a6bc74255
chelseaisgood <chelseaisgoodfmd@gmail.com>
2016-11-21 03:02:01 -0500

    updates by 2016-11-21

---
+++ Q1/PostgreSQL/create_stock.sql
@@ -0,0 +1,12 @@
+drop table if exists stock;
+
+create table stock (
+	stocksymbol INT,
+    stocktime int,
+    quantity int,
+    price int
+	primary key(stocktime)
+    );
+	
+	
+LOAD DATA INFILE 'F:/wamp64/tmp/trade.json' INTO TABLE stock.stock
